4bdd426e4bc475d3042ba44f018e7981b6c355f7
Harish Krishnan <harishkrishnan1993@gmail.com>
2023-07-19 07:17:52 +0530

    sending HTML response

--- server.mjs
+++ server.mjs
@@ -1,9 +1,19 @@
 import { createServer } from "http";
 
 const server = createServer((request, response) => {
-  response.writeHead(200, { "content-type": "text/plain; charset=utf-8" });
-  response.write("Hello");
-  response.end(" Node");
+  response.writeHead(200, { "content-type": "text/html; charset=utf-8" });
+  const htmlBody = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title>First Node App</title>
+    </head>
+    <body>
+      <h1 style="color: red">My First Node App</h1>
+    </body>
+  </html>`;
+  response.end(htmlBody);
 });
 
 server.listen(8080, () => {

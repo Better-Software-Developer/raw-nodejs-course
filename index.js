46e67e4cb6f16f9da44a1b482332e83a632a2a5d
Harish Krishnan <harishkrishnan1993@gmail.com>
2023-07-21 22:16:13 +0530

    Deleting module cache

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-import { wordCount } from "./word-count.js";
+// import { wordCount } from "./word-count.js";
 
-const sentence =
-  "Using Node.js you can create great applications that are reliable. You can also create embeddeded applications";
+// const sentence =
+//   "Using Node.js you can create great applications that are reliable. You can also create embeddeded applications";
 
-const result = wordCount(sentence);
+// const result = wordCount(sentence);
 
-for (let word in result) {
-  console.log(word + " : " + result[word]);
-}
+// for (let word in result) {
+//   console.log(word + " : " + result[word]);
+// }
+require("./testModule");
+delete require.cache[require.resolve("./testModule")];
+require("./testModule");

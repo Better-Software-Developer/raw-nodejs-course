2c444c3be778269102ca12fe987349df747c22d2
Harish Krishnan <harishkrishnan1993@gmail.com>
2023-07-21 16:28:56 +0530

    Event API and global

--- modules.mjs
+++ modules.mjs
@@ -2,26 +2,37 @@
 // console.log(uptime());
 // import { uptime } from "os";
 // console.log(uptime());
-// import { dirname } from "path";
-// import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
-// console.log(import.meta.url);
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
+console.log(import.meta.url);
+function setGlobal() {
+  global.__filename = fileURLToPath(import.meta.url);
+  global.__dirname = dirname(__filename);
+}
 
-// console.log(__filename);
-// console.log(__dirname);
+setGlobal();
+
+console.log(__filename);
+console.log(__dirname);
 // import { readFile } from "fs";
 
 // readFile("test.txt", (err, data) => {
 //   console.log(data);
 //   console.log(data.toString());
 // });
-let counter = 1;
-const interval = setInterval(() => {
-  console.log(`counter: ${counter}`);
-  counter++;
-  if (counter > 5) {
-    clearInterval(interval);
-  }
-}, 1000);
+// let counter = 1;
+// const interval = setInterval(() => {
+//   console.log(`counter: ${counter}`);
+//   counter++;
+//   if (counter > 5) {
+//     clearInterval(interval);
+//   }
+// }, 1000);
+// const evtTarget = new EventTarget();
+// evtTarget.addEventListener("loggedInEvt", (event) => {
+//   console.log(`${event.type} was called!`);
+// });
+
+// const loggedInEvt = new Event("loggedInEvt");
+// evtTarget.dispatchEvent(loggedInEvt);

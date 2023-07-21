96b2cade5bd2de33e0c2060315b801112ac96ed9
Harish Krishnan <harishkrishnan1993@gmail.com>
2023-07-21 16:07:39 +0530

    Buffer global

--- modules.mjs
+++ modules.mjs
@@ -2,12 +2,18 @@
 // console.log(uptime());
 // import { uptime } from "os";
 // console.log(uptime());
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
 
-console.log(import.meta.url);
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// console.log(import.meta.url);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
-console.log(__filename);
-console.log(__dirname);
+// console.log(__filename);
+// console.log(__dirname);
+import { readFile } from "fs";
+
+readFile("test.txt", (err, data) => {
+  console.log(data);
+  console.log(data.toString());
+});
